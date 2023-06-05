52807aac0e7e236abd2f58c31e3156183ac0886a
CarlHC91 <carlos.hc.91@gmail.com>
2023-06-05 20:51:49 +0200

    Update MainApplication.java

--- picast-server-app/src/main/java/org/raspberry/picast/MainApplication.java
+++ picast-server-app/src/main/java/org/raspberry/picast/MainApplication.java
@@ -2,8 +2,10 @@ package org.raspberry.picast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({ "org.raspberry.picast", "org.raspberry.auth" })
 public class MainApplication {
 
 	public static void main(String[] args) {
