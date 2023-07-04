8376b070e752de811f8da04e02bb2ba5776a13e5
CarlHC91 <carlos.hc.91@gmail.com>
2023-07-04 12:26:16 +0200

    Refactorización en auth-server

--- picast-server-app/src/main/java/org/raspberry/picast/security/JWTAuthorizationFilter.java
+++ picast-server-app/src/main/java/org/raspberry/picast/security/JWTAuthorizationFilter.java
@@ -9,10 +9,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.raspberry.auth.iface.UserAuthorityIface;
-import org.raspberry.auth.iface.UserSessionIface;
-import org.raspberry.auth.pojos.entities.UserAuthorityVO;
-import org.raspberry.auth.pojos.entities.UserDetailsVO;
+import org.raspberry.auth.iface.user.UserAuthorityIface;
+import org.raspberry.auth.iface.user.UserSessionIface;
+import org.raspberry.auth.pojos.entities.user.UserAuthorityVO;
+import org.raspberry.auth.pojos.entities.user.UserDetailsVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
